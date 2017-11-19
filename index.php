11807d2ecabae310d533a6a9f326350870305ce6
sokkhy <khysok95@gmail.com>
2017-11-19 22:47:26 +0700

    update git commit -mupdate

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
 </head>
 
 <body onscroll="myFunction()">
-<?php get_header(); ?>
+<!-- <?php //get_header(); ?> -->
 <?php
   // to connect to database
   $servername = "localhost";
